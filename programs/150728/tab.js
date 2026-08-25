6fca8b800227800525faab44e03c054fc746490b
names <130225200+nameswastaken@users.noreply.github.com>
2026-08-25 07:42:19 -0400

    fixes

--- programs/150728/tab.js
+++ programs/150728/tab.js
@@ -4,6 +4,6 @@ const create = document.getElementById("addtabEdge");
 function createtab() {
     const tab = document.createElement("button");
     tab.setAttribute("class", "tab");
-    tab.textContent('fat');
+    tab.innerHTML = '<span class="tabimg-Edge">&#xF5ED;</span><span>Tab</span><span class="icon close">&#xE711;</span>';
     tabbar.insertBefore(tab, create);
 }
